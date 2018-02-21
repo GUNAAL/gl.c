b653257f501f3271e7d49b7be6c7c5d3534905f4
A.GUNASEELAN <35915131+GUNAAL@users.noreply.github.com>
2018-02-21 14:14:31 +0530

    Create e.c

---
+++ e.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int s;
+printf("enter the number");
+scanf("%d",&s);
+if(s/3)
+{
+printf("yes it is power of 3");
+}
+else
+{
+printf("no it is not a power of 3");
+}
+return 0;
+}
