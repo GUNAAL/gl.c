85a7af5e7ee3a8142b63170314d48e5c78b1a145
A.GUNASEELAN <35915131+GUNAAL@users.noreply.github.com>
2018-02-23 16:37:48 +0530

    Create x.c

---
+++ x.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	char r[60];
+	int i,len;
+	printf("enter the string\n");
+	scanf("%s",r);
+	len=strlen(r);
+	for(i=0;i<len;i=i+2)
+	{
+		printf("%c",r[i]);
+	}
+	for(i=1;i<len;i=i+2)
+	{
+		printf("%c",r[i]);
+	}
+	return 0;
+}
