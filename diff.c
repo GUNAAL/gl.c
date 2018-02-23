070b7b8ac75348f9aaea17664ec9a6f82deb624e
A.GUNASEELAN <35915131+GUNAAL@users.noreply.github.com>
2018-02-23 16:47:53 +0530

    Create diff.c

---
+++ diff.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,n2;
+	printf("enter the two numbers:");
+	scanf("%d%d",&n,&n2);
+	if(n>n2)
+	{
+		printf("%d",n-n2);
+	}
+	else
+	{
+		printf("%d",n\2-n);
+ 
+	}
+}
