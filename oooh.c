9536992a8d3a64258249ed18e00a0a7c09cfbe76
A.GUNASEELAN <35915131+GUNAAL@users.noreply.github.com>
2018-02-23 16:43:26 +0530

    Create oooh.c

---
+++ oooh.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int b,c,d,e;
+	printf("enter the values");
+	scanf("%d%e",&b,&c);
+	{
+	printf("%d",b/c);
+	}
+	printf("enter the number");
+	scanf("%d%e",&d,&e);
+	{
+	printf("%d",d%e);
+	}
+	return 0;
+}
