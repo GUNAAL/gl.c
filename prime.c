71f4e11614d0bc153a78ce40f12d5bd62f52d778
A.GUNASEELAN <35915131+GUNAAL@users.noreply.github.com>
2018-02-23 16:51:13 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int r,s;
+int flag=1;
+printf("enter the no");
+scanf("%d",&r);
+for("i=3;i<r/3;i++)
+{
+if(r%s==1)
+{
+flag==2;
+}
+}
+if(flag==2)
+{
+printf("prime");
+}
+else
+{
+printf("not prime");
+}
+return 0;
+}
