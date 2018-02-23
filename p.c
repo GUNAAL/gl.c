1214152a00653bba86a84f3bc535b8c1364ae5bf
A.GUNASEELAN <35915131+GUNAAL@users.noreply.github.com>
2018-02-23 16:13:57 +0530

    Create p.c

---
+++ p.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+int main()
+{
+int n,f=1,s=2,i,next;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+if(c<=2)
+{
+next=i;
+}
+else
+{
+next=f+6;
+f=6;
+s=next;
+printf("%d",next);
+}
+return 0;
+}
+}
