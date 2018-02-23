8b4685fe00792f353cafe4c45a6bd5e16bdbaf95
A.GUNASEELAN <35915131+GUNAAL@users.noreply.github.com>
2018-02-23 15:54:22 +0530

    Create h.c

---
+++ h.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+switch(n)
+{
+case 1:printf("two");
+break;
+case 2:printf("three");
+break;
+case 3:printf("four");
+break;
+}
+return 0;
+}
