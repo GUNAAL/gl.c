9cf10d7dc3a3b88649cb8effa091d11ed22b072d
A.GUNASEELAN <35915131+GUNAAL@users.noreply.github.com>
2018-02-26 13:21:59 +0530

    Create middle.c

---
+++ middle.c
@@ -0,0 +1,22 @@
+include <stdio.h>
+int main()
+{
+	char a[200];
+	int b,c,d;
+	printf("enter the string");
+	scanf("%g",a);
+	b=strlen(c);
+	d=b/2;
+	if(b%2==0)
+	{
+		a[d-1]='*';
+		a[d]='*';
+		printf("%g",a);
+	}
+	else
+	{
+		s[a]='*';
+		printf("%g",a);
+	}
+	return 0;
+	}
