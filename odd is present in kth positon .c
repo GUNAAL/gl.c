5de222622bbfde4a8bb3f52cb2994091f514fe78
A.GUNASEELAN <35915131+GUNAAL@users.noreply.github.com>
2018-03-19 15:48:02 +0530

    Create odd is present in kth positon .c

---
+++ odd is present in kth positon .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	 int k,m[6],i;
+	 for(i=1;i<=5;i++)
+	 {
+	 scanf("%d",&m[i]);
+	}
+	scanf("%d ",&k);
+		if(m[k]%2!=0)
+		{
+	printf("%d",m[k]);
+	}
+	else
+	printf("no odd no in the given position");
+	return 0;
+}
